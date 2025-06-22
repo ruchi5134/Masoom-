04413aac960bb4a5e24d6640a060b980cf1e0b11
ruchi5134 <kn043613@gmail.com>
2025-06-22 14:20:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -16,7 +16,7 @@ BOT_TOKEN = getenv("BOT_TOKEN")
 # -------------------------------------------------------
 OWNER_USERNAME = getenv("OWNER_USERNAME","")
 # --------------------------------------------------------
-BOT_USERNAME = getenv("BOT_USERNAME" , "LavyaMusicbot")
+BOT_USERNAME = getenv("BOT_USERNAME" , "masoomX_musicbot")
 # --------------------------------------------------------
 BOT_NAME = getenv("BOT_NAME")
 # ---------------------------------------------------------
